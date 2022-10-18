1fad0e38a0d5162e3e2d4e7e798338deebe56258
stephancloete <stephancloete@gmail.com>
2022-10-18 17:24:31 +0200

    Update PosterRows.js
    fetching data

--- components/PosterRows.js
+++ components/PosterRows.js
@@ -5,24 +5,37 @@ app.component('poster-rows', {
         <h2>New & Noteworthy</h2>
         <div class="rowPosters">               
             <div class="rowPosterLarge">
-                    <img :src="image" alt="" />
-                    <p class="posterText">{{}}</p>
+            <img :src="image" alt="" />
+            <p class="posterText">{{name}}</p>
+                    
             </div>
         </div>
     </div>`,
  data() {
     return {
-      films: []
+      films: [],
+      image: './images/21-200x300.jpeg',
+      name: 'Prof. Ena Kulas Jr.'
       }
+      
   },
   /* how to fetch JSON data: https://www.youtube.com/watch?v=7iDGJolHFmU */
+
   mounted() {
     fetch('https://project-apis.codespace.co.za/api/movies')
         .then(res => res.json())
         .then(data => this.films = data)
-        .catch(err => console.log(err.message) )
+        .catch(err => console.log(err.message))
+        
   },
   methods: {
-    
-  }
+
+  },
+
+  computed: {
+    console: () => console,
+    window: () => window,
+  },
+
+  
 })
